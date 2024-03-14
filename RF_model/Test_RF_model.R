d856317f8aeaa11ebbf69c285222db4aff5abbab
UPCmctr <163435536+UPCmctr@users.noreply.github.com>
2024-03-14 18:24:01 +0100

    Update Test_RF_model.R

--- RF_model/Test_RF_model.R
+++ RF_model/Test_RF_model.R
@@ -7,7 +7,7 @@ library(ROCR)
 
 set.seed(123)
 
-# Read the optimized model from an RDS file1
+# Read the optimized model from an RDS file
 model <- readRDS("randomforest-best_model.rds")
 
 # Load the test dataset
@@ -17,30 +17,25 @@ test_set <- read.csv("test_set.csv", sep = ",", row.names = 1, header = TRUE)
 real_activity <- as.factor(test_set$activity)
 
 # Predict using the model
-# Note: It seems there was duplicated code for prediction, which is removed in this version
 predicted_activity <- predict(model, test_set)
 
 # Generate a confusion matrix
-# 'reference' is the actual outcome, 'data' is the predicted outcome
-# Adjust the prediction to match expected format if necessary
 confusion_matrix <- confusionMatrix(
-  data = as.factor(ifelse(as.numeric(predictFinal) == 1, 0, 1)),
+  data = as.factor(predicted_activity),
   reference = real_activity,
   positive = '1'
 )
 
 # Calculate various performance metrics
-# Average of Sensitivity and Specificity for Balanced Accuracy
 balAccuracy <- (confusion_matrix$byClass['Sensitivity'] + confusion_matrix$byClass['Specificity']) / 2
 
-# Matthews Correlation Coefficient (MCC)
-mccValue <- mcc(
-  TP = confusion_matrix$table[2, 2],
-  FP = confusion_matrix$table[2, 1],
-  TN = confusion_matrix$table[1, 1],
-  FN = confusion_matrix$table[1, 2]
-)
-confusion_matrix
+# Matthews Correlation Coefficient (MCC) calculation
+TP <- confusion_matrix$table[2, 2]
+FP <- confusion_matrix$table[1, 2]
+TN <- confusion_matrix$table[1, 1]
+FN <- confusion_matrix$table[2, 1]
+
+mccValue <- (TP * TN - FP * FN) / sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
 
 # Compile results into a dataframe
 results <- data.frame(
@@ -52,21 +47,22 @@ results <- data.frame(
 )
 
 # Calculate AUC
-roc_obj <- roc(response = real_activity, predictor = as.numeric(predictFinal)-1)
+roc_obj <- roc(response = real_activity, predictor = as.numeric(predicted_activity))
 auc_value <- auc(roc_obj)
 
-results$AUC=auc_value
+results$AUC = auc_value
 
 # Assign meaningful names to the rows and columns of the results dataframe
 rownames(results) <- 'External'
-colnames(results) <- c('Accuracy', 'BalAccuracy', 'Sensitivity', 'Specificity', 'MCC','AUC')
+colnames(results) <- c('Accuracy', 'BalAccuracy', 'Sensitivity', 'Specificity', 'MCC', 'AUC')
 
+results
 
-prediction_df=cbind( as.data.frame(predicted_activity),test_set$activity)
+# Prepare prediction dataframe for export
+prediction_df = cbind(as.data.frame(predicted_activity), test_set$activity)
+colnames(prediction_df) = c('Predicted activity', 'Real activity')
 
-colnames(prediction_df) = c( 'Predicted activity','Real activity')
 write.csv(prediction_df, "test_set_predicted_molecules_RF.csv")
 
-#Save the results to a CSV file
+# Save the results to a CSV file
 write.csv(results, "RF_performance_External.csv", row.names = TRUE)
-
